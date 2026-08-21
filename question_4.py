1858d3d47ecb6807b98e51008ca4fe56431939aa
Prajakta <shindeprajakta2711@gmail.com>
2026-08-21 13:04:31 +0530

    maximum occured number

--- question_4.py
+++ question_4.py
@@ -1,9 +1,16 @@
 # Occurenence of number the most of the time 
-numbers = {10, 5, 10, 20, 5, 10, 30, 20}
-highest_num = 0
-most_freq_number = 0
-for number , count in numbers.items():
+numbers = [10, 5, 10, 20, 5, 10, 30, 20]
+freq = {}
+for i in numbers:
+    if i in freq:
+        freq[i]=freq[i]+1
+    else:
+        freq[i]=1
+
+highest_num=0
+most_frequent_num=0
+for number, count in freq.items():
     if count>highest_num:
         highest_num=count
-        most_freq_number=numbers
-print(most_freq_number)
+        most_frequent_num=number
+print(most_frequent_num)
