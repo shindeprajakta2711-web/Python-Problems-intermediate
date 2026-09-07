8a69146be9f8fcc0d70901ed655ebc696db6d7d8
Prajakta <shindeprajakta2711@gmail.com>
2026-09-07 11:56:15 +0530

    number greater than

---
+++ question_5.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 2, 4, 1, 3, 2, 5, 4, 2]
+freq = {}
+for i in numbers:
+    if i in freq:
+        freq[i]=freq[i]+1
+    else:
+        freq[i]=1
+
+highest_num=0
+most_freq_num=0
+for number, count in freq.items():
+    if count>highest_num:
+        highest_num=count
+        most_freq_num=number
+print("most occured number:", most_freq_num)
+print("Freq of occurence:", highest_num)
---
+++ question_6.py
@@ -0,0 +1,6 @@
+numbers = [15, 22, 7, 40, 11, 18, 30, 5]
+new_num=[]
+for i in numbers:
+    if i>15:
+        new_num.append(i)
+print(new_num)
