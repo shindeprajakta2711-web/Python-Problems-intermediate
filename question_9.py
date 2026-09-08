88821356c4683ec20d1d2d75c33ef9bd426e6b9f
Prajakta <shindeprajakta2711@gmail.com>
2026-09-08 13:20:36 +0530

    changed one

--- question_9.py
+++ question_9.py
@@ -1,8 +1,8 @@
-# Fetching the number to reach the target by using enumerate functions
+# Fetching the number to reach the target by using enumerate function 
 numbers = [3, 2, 4, 8, 7, 5]
 target = 9
 seen = {}
-for i , sum in enumerate(numbers):
+for i , sum in enumerate(numbers):  # Enumerate function enables to take index and key value 
     required=target-sum
     if required in seen:
         print([seen[required], i])
