6173b80eebcdc6a2e09a051cae44ef1fcf30fbf1
Prajakta <shindeprajakta2711@gmail.com>
2026-09-08 13:16:17 +0530

    Index location for the sum of the target

---
+++ question_9.py
@@ -0,0 +1,10 @@
+# Fetching the number to reach the target by using enumerate function 
+numbers = [3, 2, 4, 8, 7, 5]
+target = 9
+seen = {}
+for i , sum in enumerate(numbers):
+    required=target-sum
+    if required in seen:
+        print([seen[required], i])
+        break
+    seen[sum]=i 
