1123242ed20f1d5b0f073c7f5488181f6db21c80
Prajakta <shindeprajakta2711@gmail.com>
2026-09-08 13:21:51 +0530

    final

--- question_9.py
+++ question_9.py
@@ -3,6 +3,7 @@ numbers = [3, 2, 4, 8, 7, 5]
 target = 9
 seen = {}
 for i , sum in enumerate(numbers):  # Enumerate function enables to take index and key value 
+# these gives the dict type format without the use of the loop to run 
     required=target-sum
     if required in seen:
         print([seen[required], i])
