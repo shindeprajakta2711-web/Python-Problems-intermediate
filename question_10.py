e6465edae67877504c6a11649172945898e989ef
Prajakta <shindeprajakta2711@gmail.com>
2026-09-09 10:47:17 +0530

    updated code

--- question_10.py
+++ question_10.py
@@ -1,15 +1,24 @@
-sales = [
-    ("Laptop", 50000),
-    ("Mouse", 800),
-    ("Laptop", 50000),
-    ("Keyboard", 1500),
-    ("Mouse", 800),
-    ("Laptop", 50000)
-]
+# sales = [
+#     ("Laptop", 50000),
+#     ("Mouse", 800),
+#     ("Laptop", 50000),
+#     ("Keyboard", 1500),
+#     ("Mouse", 800),
+#     ("Laptop", 50000)
+# ]
+# count={}
+# for product, price in sales:
+#     if product in count:
+#         count[product]+=1
+#     else:
+#         count[product]=1
+# print(count)
+
+words = ["apple", "banana", "apple", "orange", "banana", "apple"]
 count={}
-for product, price in sales:
-    if product in count:
-        count[product]+=1
+for fruits in words:
+    if fruits in count:
+        count[fruits]+=1
     else:
-        count[product]=1
+        count[fruits]=1
 print(count)
