8d86c415d7cb2a2e2bc0ea80288c556e84f2c659
Prajakta <shindeprajakta2711@gmail.com>
2026-09-15 11:45:23 +0530

    New one added

--- question_12.py
+++ question_12.py
@@ -1,9 +1,11 @@
-# First repeated number
-numbers = [4, 7, 2, 9, 7, 5, 2]
-seen=set()
-for num in numbers:
-    if num in seen:
-        print("First repeated number is :", num)
-        break
-    else:
-        seen.add(num)
+# # First repeated number
+# numbers = [4, 7, 2, 9, 7, 5, 2]
+# seen=set()
+# for num in numbers:
+#     if num in seen:
+#         print("First repeated number is :", num)
+#         break
+#     else:
+#         seen.add(num)
+
+
