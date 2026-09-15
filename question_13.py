b2a72e92f15606d140a595785a2b5b7bfde4fc26
Prajakta <shindeprajakta2711@gmail.com>
2026-09-15 12:12:23 +0530

    frequency count of words

--- question_13.py
+++ question_13.py
@@ -1,9 +1,13 @@
-# frequency count of the words:
-words = ["apple", "banana", "apple", "orange", "banana", "apple"]
-count = {}
-for word in words:
-    if word in count:
-        count[word]+=1
-    else:
-        count[word]=1
-print(count)
+# # frequency count of the words:
+# words = ["apple", "banana", "apple", "orange", "banana", "apple"]
+# count = {}
+# for word in words:
+#     if word in count:
+#         count[word]+=1
+#     else:
+#         count[word]=1
+# print(count)
+
+
+# Printing the most occurrent name and the count 
+words = ["python","java","python","sql","java","python","sql"]
