89ac56075b50939d2c55942f25c2f1afcfa6f359
Prajakta <shindeprajakta2711@gmail.com>
2026-09-15 11:54:39 +0530

    Frequency count of every word

--- question_13.py
+++ question_13.py
@@ -1,1 +1,9 @@
 # frequency count of the words:
+words = ["apple", "banana", "apple", "orange", "banana", "apple"]
+count = {}
+for word in words:
+    if word in count:
+        count[word]+=1
+    else:
+        count[word]=1
+print(count)
