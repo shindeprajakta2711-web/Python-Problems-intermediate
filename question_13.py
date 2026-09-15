55acdf4e8da243e3f59063adb365e66db57b7863
Prajakta <shindeprajakta2711@gmail.com>
2026-09-15 13:19:19 +0530

    Most frequent occurred word and its count

--- question_13.py
+++ question_13.py
@@ -11,3 +11,17 @@
 
 # Printing the most occurrent name and the count 
 words = ["python","java","python","sql","java","python","sql"]
+count={}
+for word in words:
+    if word in count:
+        count[word]+=1
+    else:
+        count[word]=1
+highest=0
+result=""
+for name, occurrence in count.items():
+    if occurrence>highest:
+        highest=occurrence
+        result=name
+print("most frequent name: ",result)
+print("Frequency:", highest)
