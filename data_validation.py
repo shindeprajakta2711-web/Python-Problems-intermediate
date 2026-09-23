734f0fc2d1815469bd6783baaaf4bde7fefa2c72
Prajakta <shindeprajakta2711@gmail.com>
2026-09-23 15:40:06 +0530

    data

--- data_validation.py
+++ data_validation.py
@@ -1,1 +1,10 @@
-# nejkofr
+ages = [25, 31, -5, 42, 150, 28, 0, 35]
+valid = []
+invalid = []
+for age in ages:
+    if age>=1 and age<=100:
+        valid.append(age)
+    else:
+        invalid.append(age)
+print("Valid ages: ", valid)
+print("Invalid ages: ", invalid)
